44fb46669adde3a3fc837a8dc63d1b7b208be0cf
Alabbas Alhaj Ali <weeamha@gmail.com>
2018-06-18 16:04:08 +0200

    init the clusters

--- CapReader/src/his/packet/clustring/Main.java
+++ CapReader/src/his/packet/clustring/Main.java
@@ -9,37 +9,81 @@ import io.pkts.framer.FramingException;
 
 public class Main {
 
+	public static String[] streams = { "trafic1_paylod", "trafic2_paylod", "trafic3_paylod", "trafic4_paylod", "trafic5_paylod" };
+	
 	public static void main(String[] args) throws IOException, FramingException {
 
 		CapReader reader = new CapReader();
 		String[] files = { "trafic1.pcap", "trafic2.pcap", "trafic3.pcap", "trafic4.pcap", "trafic5.pcap" };
 		reader.readFiles(files);
 
-		String[] streams = { "trafic1_paylod", "trafic2_paylod", "trafic3_paylod", "trafic4_paylod", "trafic5_paylod" };
-
-		List<Cluster> clusterList = new ArrayList<Cluster>();
-		
-		SerialStreamReader serialStreams = new SerialStreamReader(streams);
-		
-		Cluster httpCluster = new Cluster("http");
-		String httpbacket = "";
-		while ((httpbacket = serialStreams.hasNext()) != null) {
-			httpCluster.addFeatures(Cluster.getProfile(httpbacket, 2));
-			httpCluster.addFeatures(Cluster.getProfile(httpbacket, 3));
-			httpCluster.addFeatures(Cluster.getProfile(httpbacket, 4));
-		}
-		
-		clusterList.add(httpCluster);
-		
-		Cluster httpCluster = new Cluster("http");
+		List<Cluster> clusterList = getClasters();
 		
 		RandomStreamReader randomStreams = new RandomStreamReader(streams);
-
 		String randombacket;
 		while ((randombacket = randomStreams.hasNext()) != null) {
 			
 		}
-
+	}
+	
+	public static List<Cluster> getClasters() throws IOException {
+		List<Cluster> clusterList = new ArrayList<Cluster>();
+		
+		SerialStreamReader serialStreams = new SerialStreamReader(streams);
+		
+		Cluster rtpCluster = new Cluster("rtp");
+		String rtpbacket = "";
+		while ((rtpbacket = serialStreams.hasNextRtp()) != null) {
+			rtpCluster.addFeatures(Cluster.getProfile(rtpbacket, 2));
+			rtpCluster.addFeatures(Cluster.getProfile(rtpbacket, 3));
+			rtpCluster.addFeatures(Cluster.getProfile(rtpbacket, 4));
+			rtpCluster.addFeatures(Cluster.getProfile(rtpbacket, 5));
+		}
+		clusterList.add(rtpCluster);
+		
+		Cluster sipCluster = new Cluster("sip");
+		String sipbacket = "";
+		while ((sipbacket = serialStreams.hasNextSip()) != null) {
+			sipCluster.addFeatures(Cluster.getProfile(sipbacket, 2));
+			sipCluster.addFeatures(Cluster.getProfile(sipbacket, 3));
+			sipCluster.addFeatures(Cluster.getProfile(sipbacket, 4));
+			sipCluster.addFeatures(Cluster.getProfile(sipbacket, 5));
+		}
+		clusterList.add(sipCluster);
+		
+		
+		Cluster rtcpCluster = new Cluster("sip");
+		String rtcpbacket = "";
+		while ((rtcpbacket = serialStreams.hasNextRtcp()) != null) {
+			rtcpCluster.addFeatures(Cluster.getProfile(rtcpbacket, 2));
+			rtcpCluster.addFeatures(Cluster.getProfile(rtcpbacket, 3));
+			rtcpCluster.addFeatures(Cluster.getProfile(rtcpbacket, 4));
+			rtcpCluster.addFeatures(Cluster.getProfile(rtcpbacket, 5));
+		}
+		clusterList.add(rtcpCluster);
+		
+		
+		Cluster sdpCluster = new Cluster("sdp");
+		String sdpbacket = "";
+		while ((sdpbacket = serialStreams.hasNextSdp()) != null) {
+			sdpCluster.addFeatures(Cluster.getProfile(sdpbacket, 2));
+			sdpCluster.addFeatures(Cluster.getProfile(sdpbacket, 3));
+			sdpCluster.addFeatures(Cluster.getProfile(sdpbacket, 4));
+			sdpCluster.addFeatures(Cluster.getProfile(sdpbacket, 5));
+		}
+		clusterList.add(sdpCluster);
+		
+		Cluster httpCluster = new Cluster("http");
+		String httpbacket = "";
+		while ((httpbacket = serialStreams.hasNextHttp()) != null) {
+			httpCluster.addFeatures(Cluster.getProfile(httpbacket, 2));
+			httpCluster.addFeatures(Cluster.getProfile(httpbacket, 3));
+			httpCluster.addFeatures(Cluster.getProfile(httpbacket, 4));
+			httpCluster.addFeatures(Cluster.getProfile(httpbacket, 5));
+		}
+		clusterList.add(httpCluster);
+		
+		return clusterList;
 	}
 
 }
--- CapReader/src/his/packet/clustring/SerialStreamReader.java
+++ CapReader/src/his/packet/clustring/SerialStreamReader.java
@@ -5,50 +5,76 @@ import java.io.IOException;
 
 public class SerialStreamReader extends MyStreamReader {
 
-	private int currentFolder = 0;
 	private int currentHttpFolder = 0;
-	private int currentFile = 0;
+	private int currentRtpFolder = 0;
+	private int currentSipFolder = 0;
+	private int currentRtcpFolder = 0;
+	private int currentSdpFolder = 0;
 
 	public SerialStreamReader(String[] folers) throws FileNotFoundException {
 		super(folers);
 	}
-
-	public String hasNext() throws IOException {
+	
+	public String hasNextRtp() throws IOException {
 		String line = "";
-
-		if ((line = readers[currentFolder][currentFile].readLine()) != null) {
+		if ((line = readers[currentRtpFolder][0].readLine()) != null) {
 			return line;
 		}
-
-		currentFile++;
-
-		if (currentFile == hight && currentFolder == width) {
+		currentRtpFolder ++;
+		if(currentRtpFolder == width) {
 			return null;
 		}
+		return hasNextRtp();
+	}
 
-		if (currentFile == hight) {
-			currentFile = 0;
-			currentFolder++;
+	public String hasNextSip() throws IOException {
+		String line = "";
+		if ((line = readers[currentSipFolder][1].readLine()) != null) {
+			return line;
 		}
+		currentSipFolder ++;
+		if(currentSipFolder == width) {
+			return null;
+		}
+		return hasNextSip();
+	}
 
-		return hasNext();
+	public String hasNextRtcp() throws IOException {
+		String line = "";
+		if ((line = readers[currentRtcpFolder][2].readLine()) != null) {
+			return line;
+		}
+		currentRtcpFolder ++;
+		if(currentRtcpFolder == width) {
+			return null;
+		}
+		return hasNextRtcp();
+	}
+	
+	public String hasNextSdp() throws IOException {
+		String line = "";
+		if ((line = readers[currentSdpFolder][3].readLine()) != null) {
+			return line;
+		}
+		currentSdpFolder ++;
+		if(currentSdpFolder == width) {
+			return null;
+		}
+		return hasNextSdp();
 	}
 	
 	public String hasNextHttp() throws IOException {
-		
 		String line = "";
-		
 		if ((line = readers[currentHttpFolder][4].readLine()) != null) {
 			return line;
 		}
-		
 		currentHttpFolder ++;
-		
 		if(currentHttpFolder == width) {
 			return null;
 		}
-		
 		return hasNextHttp();
 	}
-
+	
 }
+
+
