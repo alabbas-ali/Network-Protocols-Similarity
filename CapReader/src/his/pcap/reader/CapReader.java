2cd2f2b7dc169f829a714488899613a8e15f9c8b
Alabbas Alhaj Ali <weeamha@gmail.com>
2018-06-21 14:17:53 +0200

    pcap new way to read the files

--- CapReader/src/his/pcap/reader/CapReader.java
+++ CapReader/src/his/pcap/reader/CapReader.java
@@ -2,6 +2,11 @@ package his.pcap.reader;
 
 import java.io.IOException;
 
+import org.pcap4j.core.PcapHandle;
+import org.pcap4j.core.PcapHandle.TimestampPrecision;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.Pcaps;
+
 import io.pkts.Pcap;
 import io.pkts.framer.FramingException;
 
@@ -14,6 +19,27 @@ public class CapReader {
 			Pcap pcap = Pcap.openStream(file);
 			pcap.loop(new MyPacketHandler(folder, "trafic" + (i + 1)));
 			pcap.close();
+			
+			
 		}
 	}
+	
+	public static void main(String[] args) throws IOException, FramingException, PcapNativeException {
+
+		String[] streams = { "resources/clustring/trafic1_paylod", "resources/clustring/trafic2_paylod",
+				"resources/clustring/trafic3_paylod", "resources/clustring/trafic4_paylod",
+				"resources/clustring/trafic5_paylod" };
+		String[] files = { "trafic1.pcap", "trafic2.pcap", "trafic3.pcap", "trafic4.pcap", "trafic5.pcap" };
+
+		System.out.println("::: Read pcap files and extract protocol packets :::");
+
+		PcapHandle handle;
+
+		try {
+			handle = Pcaps.openOffline(files[0], TimestampPrecision.NANO);
+		} catch (PcapNativeException e) {
+			handle = Pcaps.openOffline(files[0]);
+		}
+	}
+	
 }
