861f6608f01a83711716ac888e04c7ad727b487b
Alabbas Alhaj Ali <weeamha@gmail.com>
2018-07-19 14:39:29 +0200

    rite the shell script to capture trafic and do (http and ftp) requists

--- scipt.sh
+++ scipt.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 # A simple script
 
+if [[ ( -z "$1" ) || ( -z "$2" ) || ( -z "$3" ) || ( -z "$4" ) ]] 
+then
+    printf "No argument supplied";
+	printf "Please Use the script like :";
+	printf "scipt.sh ethernet_port server_IP ftp_user ftp_password";
+	exit 0;
+fi
+
+hash tshark 2>/dev/null || {
+	printf >&2 "Installing tshark.  please waite.";
+	sudo apt install tshark
+}
+
+hash curl 2>/dev/null || {
+	printf >&2 "Installing curl.  please waite.";
+	sudo apt install curl
+}
+
 declare -A randoms
 
-for i in {1..50} 
+# random_init() the strings for SIP conversation
+for i in {1..50}
 do
-    randoms[random_verb$i]=$(shuf -i 0-1000 -n 1 | md5sum)
+	randoms[random_verb$i]=$(shuf -i 0-1000 -n 1 | md5sum)
 	randoms[random_thing$i]=$(shuf -i 0-1000 -n 1 | md5sum)
 	randoms[random_name$i]=$(shuf -i 0-1000 -n 1 | md5sum)
 	randoms[random_study$i]=$(shuf -i 0-1000 -n 1 | md5sum)
@@ -22,12 +41,34 @@ do
 	randoms[random_location$i]=$(shuf -i 0-1000 -n 1 | md5sum)
 done
 
-for j in {1..2} 
+
+# Start runing the 150 experments
+for j in {1..3}
 do
-	for j in {1..50} 
+	# remove the directory $j if exist and create new one
+	[ ! -d $j ] || rm -rf $j;
+	mkdir $j;
+	
+	for i in {1..50}
 	do
-		echo ${randoms[random_verb$j]}
-		echo ${randoms[random_thing$j]}
+		# Start capture of ethernet_port 
+		printf "\nStart experiment $j captures traffic $i. \n";
+		tshark -i $1 -w $j/traffic$i.pcap -a duration:12 & 
+		sleep 2;
+		# do HTTP curl request to web page J with parameter I;
+		printf "\ncurl to http://$2/webpage$j?id=$i \n";
+		curl -i -H "Accept: application/html" \
+				-H "Content-Type: application/html" \
+				-X GET http://$2/webpage$j?id=$i >> http_output.txt
+		# do FTP request to folderJ file I; 
+		mkdir ftptmp;
+		wget -c ftp://$3:$4@$2/folder$j/file$i.docx \
+				-p /ftptmp/
+		rm -rf ftptmp;
+		#echo ${randoms[random_verb$j]};
+		#echo ${randoms[random_thing$j]};
+		
+		sleep 10;
 	done
 done
 
